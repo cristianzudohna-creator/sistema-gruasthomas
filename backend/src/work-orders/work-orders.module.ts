2a0693036142e4616250e289b1daaf80f3ec753c
Cristian Zudohna <cristian.zudohna@gmail.com>
2026-04-01 11:11:48 -0300

    firebase multi device ot notificacion produccion

--- backend/src/work-orders/work-orders.module.ts
+++ backend/src/work-orders/work-orders.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
 import { AuditModule } from "../audit/audit.module";
+import { FirebaseModule } from "../firebase/firebase.module";
 
 import { WorkOrdersController } from "./work-orders.controller";
 import { WorkOrdersService } from "./work-orders.service";
 
 @Module({
-  imports: [PrismaModule, AuditModule],
+  imports: [PrismaModule, AuditModule, FirebaseModule],
   controllers: [WorkOrdersController],
   providers: [WorkOrdersService],
   exports: [WorkOrdersService],
